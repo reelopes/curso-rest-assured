685a42264f2a31c49e8718483b37cdab68f4a0de
renanlopesgoncalves <ree.lopes@hotmail.com>
2024-09-12 10:48:17 -0300

    Conteúdo até aula 35.

--- src/main/java/br/ce/wcaquino/rest/VerbosTest.java
+++ src/main/java/br/ce/wcaquino/rest/VerbosTest.java
@@ -1,7 +1,13 @@
 package br.ce.wcaquino.rest;
 
+import io.restassured.RestAssured;
+import io.restassured.config.EncoderConfig;
 import io.restassured.http.ContentType;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -42,6 +48,67 @@ public class VerbosTest {
 	}
 
 	@Test
+	public void deveSalvarUsuarioUsandoMap() {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("name", "Usuário via map");
+		params.put("age", 25);
+
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body(params)
+		.when()
+			.post("https://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.body("id", is(notNullValue()))
+			.body("name", is("Usuário via map"))
+			.body("age", is(25))
+		;
+	}
+
+	@Test
+	public void deveSalvarUsuarioUsandoObjeto() {
+		User user = new User("Usuário via objeto", 35);
+
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body(user)
+		.when()
+			.post("https://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.body("id", is(notNullValue()))
+			.body("name", is("Usuário via objeto"))
+			.body("age", is(35))
+		;
+	}
+	@Test
+	public void deveDeserializarObjetoAoSalvarUsuario() {
+		User user = new User("Usuário deserializado", 35);
+
+		User usuarioInserido = given()
+			.log().all()
+			.contentType("application/json")
+			.body(user)
+		.when()
+			.post("https://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.extract().body().as(User.class)
+		;
+
+//		System.out.print(usuarioInserido);
+		Assert.assertEquals("Usuário deserializado", usuarioInserido.getName());
+		Assert.assertThat(usuarioInserido.getAge(), is((35)));
+		Assert.assertThat(usuarioInserido.getId(), notNullValue());
+	}
+
+	@Test
 	public void deveSalvarUsuarioXML() {
 		given()
 			.log().all()
@@ -55,6 +122,130 @@ public class VerbosTest {
 			.body("user.@id", is(notNullValue()))
 			.body("user.name", is("Jose"))
 			.body("user.age", is("50"))
+		;
+	}
+
+	@Test
+	public void deveSalvarUsuarioviaXMLUsandoObjeto() {
+		User user = new User("Usuario XML", 40);
+
+		given()
+			.log().all()
+			.contentType(ContentType.XML)
+//			.contentType(ContentType.XML+";charset=UTF-8" ) // para funcionar com acento (e.g. usuário)
+			.body(user)
+		.when()
+			.post("https://restapi.wcaquino.me/usersXML")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.body("user.@id", is(notNullValue()))
+			.body("user.name", is("Usuario XML"))
+			.body("user.age", is("40"))
+		;
+	}
+
+	@Test
+	public void deveDeserializarXMLAoSalvarUsuario() {
+		User user = new User("Usuario XML", 40);
+
+		User usuarioInserido = given()
+			.log().all()
+			.contentType(ContentType.XML)
+			.body(user)
+		.when()
+			.post("https://restapi.wcaquino.me/usersXML")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.extract().body().as(User.class)
+		;
+
+//		System.out.print(usuarioInserido);
+		Assert.assertThat(usuarioInserido.getId(), notNullValue());
+		Assert.assertThat(usuarioInserido.getName(), is("Usuario XML"));
+		Assert.assertThat(usuarioInserido.getAge(), is(40));
+		Assert.assertThat(usuarioInserido.getSalary(), nullValue());
+	}
+
+	@Test
+	public void deveAlterarUsuario() {
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body("{\"name\":\"Usuario alterado\",\"age\":80}")
+		.when()
+			.put("https://restapi.wcaquino.me/users/1")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("name", is("Usuario alterado"))
+			.body("age", is(80))
+			.body("salary", is(1234.5678f))
+		;
+	}
+
+	@Test
+	public void deveCustomizarURL() {
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body("{\"name\":\"Usuario alterado\",\"age\":80}")
+		.when()
+			.put("https://restapi.wcaquino.me/{entidade}/{userId}", "users", "1")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("name", is("Usuario alterado"))
+			.body("age", is(80))
+			.body("salary", is(1234.5678f))
+		;
+	}
+
+	@Test
+	public void deveCustomizarURL2() {
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body("{\"name\":\"Usuario alterado\",\"age\":80}")
+				.pathParam("entidade", "users")
+				.pathParam("userId", 1)
+		.when()
+			.put("https://restapi.wcaquino.me/{entidade}/{userId}")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("name", is("Usuario alterado"))
+			.body("age", is(80))
+			.body("salary", is(1234.5678f))
+		;
+	}
+
+	@Test
+	public void deveRemoverUsuario() {
+		given()
+			.log().all()
+		.when()
+			.delete("https://restapi.wcaquino.me/users/1")
+		.then()
+			.log().all()
+			.statusCode(204)
+		;
+	}
+
+	@Test
+	public void naoDeveRemoverUsuario() {
+		given()
+			.log().all()
+		.when()
+			.delete("https://restapi.wcaquino.me/users/1000")
+		.then()
+			.log().all()
+			.statusCode(400 )
+			.body("error", is("Registro inexistente"))
 		;
 	}
 }
